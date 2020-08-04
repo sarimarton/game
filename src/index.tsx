00319cd6e66c5b3a5167221972b1992a72eeb989
sarimarton <sarimarton@gmail.com>
2020-08-05 01:37:36 +0200

    Further boilerplate and viewport

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Viewport from 'src/view/Viewport'
+
+ReactDOM.render(
+  <Viewport />,
+  document.querySelector('#root')
+)
---
+++ src/view/Viewport.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Viewport() {
+  return <div className='viewport'>hi</div>
+}
