c55f80e34ada5268cad4c82db04f9bd70f103ac5
sarimarton <sarimarton@gmail.com>
2020-08-05 02:31:40 +0200

    Add styled-components

--- src/view/Viewport.tsx
+++ src/view/Viewport.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import { useMachine } from '@xstate/react'
 import gameMachine from 'src/view/machine/machine'
 
+import styled from 'styled-components'
+
+const _div = styled('div')`
+  &&& {
+    width: 800px;
+    height: 600px;
+    background: rgba(255, 0, 0, 0.24);
+  }
+`
+
 export default function Viewport() {
   const [current, send] = useMachine(gameMachine)
 
-  return <div className="viewport">
-    <pre>
-      {JSON.stringify(current.value)}
-    </pre>
-  </div>
+  return (
+    <_div>
+      <pre>{JSON.stringify(current.value)}</pre>
+    </_div>
+  )
 }
